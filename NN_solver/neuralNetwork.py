265f94332abfd3605f664238ee8f433c0ff80bd5
AlaaBejaoui <alaabejaoui1@gmail.com>
2020-04-14 23:13:17 -0400

    fix batch training and data shuffeling for better performance

--- NN_solver/neuralNetwork.py
+++ NN_solver/neuralNetwork.py
@@ -1,22 +1,32 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as Fun
 
 class Net(nn.Module):
 
-    def __init__(self):
+    def __init__(self, numHiddenLayer=1, numUnits=10, activation="relu"):
         super(Net, self).__init__()
         
-        # an affine operation: y = Wx + b
-        self.inputLayer = nn.Linear(1, 10)  
-        self.hiddenLayer = nn.Linear(10, 10)
-        self.outputLayer = nn.Linear(10, 1)
+        self.inputLayer = nn.Linear(1, numUnits)  
+        self.hiddenLayer = nn.Linear(numUnits, numUnits)
+        self.outputLayer = nn.Linear(numUnits, 1)
+        self.numHiddenLayer = numHiddenLayer
+        if activation == "relu":
+            self.activation = nn.ReLU()
+        elif activation == "leakyReLU":
+            self.activation = nn.LeakyReLU()
+        elif activation == "sigmoid": 
+            self.activation = nn.Sigmoid()
+        else:
+            raise NotImplementedError
+        
+
 
     def forward(self, x):
         
-        x = Fun.relu(self.inputLayer(x))
-        x = Fun.relu(self.hiddenLayer(x))
-        x = Fun.relu(self.hiddenLayer(x))
-        x = Fun.relu(self.hiddenLayer(x))
+        x = self.inputLayer(x)
+        x = self.activation(x)
+        for _ in range(self.numHiddenLayer):
+            x = self.hiddenLayer(x)
+            x = self.activation(x)
         x = self.outputLayer(x)
         return x
