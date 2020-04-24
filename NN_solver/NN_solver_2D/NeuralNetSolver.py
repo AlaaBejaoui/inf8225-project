244b2a83429a97ddee6292ef68224990ff7cddf2
AlaaBejaoui <alaabejaoui1@gmail.com>
2020-04-23 23:11:16 -0400

    fix zero grad 2D

--- NN_solver/NN_solver_2D/NeuralNetSolver.py
+++ NN_solver/NN_solver_2D/NeuralNetSolver.py
@@ -58,14 +58,14 @@ class NeuralNetSolver:
 
     @property
     def training_data(self):
-        return [(x_training,y_training) for x_training, y_training in zip(self.training_grid[0].reshape(-1),self.training_grid[1].reshape(-1))]
+        return [(x_training, y_training) for x_training, y_training in zip(self.training_grid[0].reshape(-1), self.training_grid[1].reshape(-1))]
 
     @property
     def testing_data(self):
-        return [(x_testing,y_testing) for x_testing, y_testing in zip(self.testing_grid[0].reshape(-1),self.testing_grid[1].reshape(-1))]
+        return [(x_testing, y_testing) for x_testing, y_testing in zip(self.testing_grid[0].reshape(-1), self.testing_grid[1].reshape(-1))]
 
     @property
-    def training_loader(self):
+    def __training_loader(self):
         return DataLoader(self.training_data, batch_size=self.batch_size, shuffle=self.shuffle)
 
     @property
@@ -75,19 +75,21 @@ class NeuralNetSolver:
     @time_it
     def solve(self):
         criterion = nn.MSELoss()
-        optimizer = optim.SGD(self.net.parameters(), lr=self.lr)
-
+        optimizer = optim.SGD(self.net.parameters(), lr=self.lr, momentum=0.9, nesterov=True)
+        
         for epoch in range(self.numEpochs):
 
-            for batch_index, batch in enumerate(self.__training_loader):
-
+            for batch_index, (x_batch, y_batch) in enumerate(self.__training_loader):
+                # print(x_batch,y_batch)
                 loss = torch.zeros([1])
-                for x in batch:
+                for x, y in zip(x_batch, y_batch):
                     x.resize_((1, 1))
+                    y.resize_((1, 1))
                     x.requires_grad_(True)
-                    G_ = self.G(x)
+                    y.requires_grad_(True)
+                    G_ = self.G(x, y)
                     loss += criterion(G_, torch.tensor([0.]))
-                loss /= len(batch)
+                loss /= len(x_batch)
                 self.loss_array = np.append(self.loss_array, loss.item())
 
                 optimizer.zero_grad()
@@ -96,69 +98,91 @@ class NeuralNetSolver:
                 print(
                     f"epoch: {epoch} - batch: {batch_index} - loss:{loss.item():.3e}")
 
-    def psi_trial(self, x):
-        return (self.psi_hat(x) + self.F(x) * self.net(x))
+    def psi_trial(self, x, y):
+        return (self.psi_hat(x, y) + self.F(x, y) * self.net(torch.tensor([x, y])))
 
-    def dpsiTrial_dx(self, x):
-        psi_trial_ = self.psi_trial(x)
+    def dpsiTrial_dx(self, x, y):
+        if x.grad != None and y.grad != None:
+            x.grad.data.zero_()
+            y.grad.data.zero_()
+        psi_trial_ = self.psi_trial(x, y)
         psi_trial_.backward()
         return x.grad
 
-    def G(self, x):
-        psi_trial_ = self.psi_trial(x)
-        dpsiTrial_dx_ = self.dpsiTrial_dx(x)
-        return self.diff_equation(x, dpsiTrial_dx_, psi_trial_)
+    def dpsiTrial_dy(self, x, y):
+        if x.grad != None and y.grad != None:
+            x.grad.data.zero_()
+            y.grad.data.zero_()
+        psi_trial_ = self.psi_trial(x, y)
+        psi_trial_.backward()
+        return y.grad
+
+    def G(self, x, y):
+        psi_trial_ = self.psi_trial(x, y)
+        dpsiTrial_dx_ = self.dpsiTrial_dx(x, y)
+        dpsiTrial_dy_ = self.dpsiTrial_dy(x, y)
+        return self.diff_equation(x, y, dpsiTrial_dx_, dpsiTrial_dy_, psi_trial_)
 
     def plot(self):
+    
+        # NN solution
+        fig = plt.figure(figsize=(4, 4))
 
-        for x_test in self.__testing_loader:
-            self.psi_trial_array = np.append(
-                self.psi_trial_array, self.psi_trial(x_test).item())
+        Z_NN = torch.zeros_like(self.testing_grid[0])
+        for line in range(Z_NN.shape[0]):
+            for column in range(Z_NN.shape[1]):
+                # print(self.testing_grid[0][line, column])
+                # print()
+                # print(self.testing_grid[1][line, column])
+                Z_NN[line, column] = self.psi_trial(
+                    self.testing_grid[0][line, column], self.testing_grid[1][line, column])
 
-        # comparing the two solutions
-        plt.figure()
-        plt.semilogy(self.loss_array)
-        plt.xlabel("iter")
-        plt.ylabel("loss")
+        left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+        ax = fig.add_axes([left, bottom, width, height])
+        cp = ax.contour(self.testing_grid[0].detach().numpy(
+        ), self.testing_grid[1].detach().numpy(), Z_NN.detach().numpy(), 20)
+        ax.clabel(cp, inline=True,
+                  fontsize=10)
+        ax.set_title('Contour Plot of the NN solution')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
 
-        # comparing the two solutions
-        plt.figure()
-        plt.plot(self.testing_data, self.psi_trial_array, 'r', label='NN')
-        plt.xlabel("x")
-        plt.ylabel("y(x)")
-        plt.plot(self.testing_data, self.exact_solution(
-            self.testing_data), 'k', label='exact')
-        plt.legend()
-
-        # error plot
-        plt.figure()
-        err = self.exact_solution(self.testing_data) - self.psi_trial_array
-        plt.plot(self.testing_data, np.abs(err), 'k')
-        plt.xlabel("x")
-        plt.ylabel("|err(x)|")
-
+        # exact solution contour plot
+        fig = plt.figure(figsize=(4, 4))
+        Z_exact = self.exact_solution(
+            self.testing_grid[0], self.testing_grid[1])
+        left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+        ax = fig.add_axes([left, bottom, width, height])
+        cp = ax.contour(self.testing_grid[0],
+                        self.testing_grid[1], Z_exact, 20)
+        ax.clabel(cp, inline=True,
+                  fontsize=10)
+        ax.set_title('Contour Plot of the exact solution')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
         plt.show()
 
     # exact solution
-    def exact_solution(self, x):
+
+    def exact_solution(self, x, y):
         if not(self.exact_solution_):
             raise Exception("exact solution not set yet!")
         return eval(self.exact_solution_)
 
     # differential equation to solve
-    def diff_equation(self, x, dpsi_dx, psi):
+    def diff_equation(self, x, y, dpsi_dx, dpsi_dy, psi):
         if not(self.diff_equation_):
             raise Exception("differential equation not set yet!")
         return eval(self.diff_equation_)
 
     # function that constraints the neural network
-    def F(self, x):
+    def F(self, x, y):
         if not(self.F_):
             raise Exception("function F not set yet!")
-        return eval(self.F_) 
+        return eval(self.F_)
 
     # function that satisfies the boundary conditions
-    def psi_hat(self, x):
+    def psi_hat(self, x, y):
         if not(self.psi_hat_):
             raise Exception("function psi hat not set yet!")
         return eval(self.psi_hat_)
--- NN_solver/NN_solver_2D/simulation.py
+++ NN_solver/NN_solver_2D/simulation.py
@@ -4,14 +4,43 @@ from timing import time_it
 
 torch.manual_seed(0)
 
-solver = NeuralNetSolver(numHiddenLayer=0, numUnits=10, activation="leakyReLU", 
-                        numEpochs=500, batch_size=5, lr=0.03, 
-                        x_start=0, x_end=2, y_start=0, y_end=2, training_steps=5, 
-                        testing_steps=3, 
-                        shuffle=False) ##chabe true
+solver = NeuralNetSolver(numHiddenLayer=10, numUnits=5, activation="relu",
+                         numEpochs=50, batch_size=1, lr=10,
+                         x_start=0.5, x_end=1, y_start=0, y_end=1, training_steps=16,
+                         testing_steps=50,
+                         shuffle=True)
 
+solver.set_diff_equation("x * dpsi_dx + dpsi_dy - x * y")
+solver.set_psi_hat("torch.pow(x, 2) + torch.exp(-torch.pow(x, 2))")
+solver.set_F("y")
 
-print(solver.training_data)
+# x=torch.tensor([1.],requires_grad=True)
+# y=torch.tensor([1.],requires_grad=True)
+# xdx=torch.tensor([1.+1e-04],requires_grad=True)
+# ydy=torch.tensor([1.+1e-04],requires_grad=True)
 
-for batch_index, (batch1,batch2) in enumerate(solver.training_loader): #chage to __trainig_loader
-    print(batch1,batch2)
+# print(solver.psi_trial(x,y))
+# print(solver.psi_trial(xdx,y))
+# print(solver.dpsiTrial_dx(x,y))
+# print()
+# print(solver.psi_trial(x,y))
+# print(solver.psi_trial(x,ydy))
+# print(solver.dpsiTrial_dy(x,y))
+
+solver.solve()
+solver.set_exact_solution(
+    "x * (y-1) + x*x*np.exp(-2*y) + np.exp(-x*x*np.exp(-2*y)) + x*np.exp(-y)")
+
+solver.plot()
+
+# import torch
+
+# x = torch.tensor([[1,2,3,4,5],
+#         [6,7,8,9,10],
+#         [11,12,13,14,15],
+#         [0., 0., 0., 0., 0.],
+#         [0., 0., 0., 0., 0.]])
+
+# x[2,3] = 56.
+
+# print(x)
